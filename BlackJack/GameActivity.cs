f9f74a6913e75e250532546116a2b9c5d639ec74
Daniel Hayes <daniel.hayes@aviva.com>
2018-04-19 15:45:46 +0100

    implemented game functions

--- BlackJack/GameActivity.cs
+++ BlackJack/GameActivity.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.OS;
 using Android.Widget;
+using DeckOfCards;
 
 namespace BlackJack
 {
     [Activity(Label = "Game", Theme = "@android:style/Theme.Holo.NoActionBar.Fullscreen")]
     public class GameActivity : Activity
     {
+        private PlayingCardDeck Deck;
+        private List<Card> PlayersHand = new List<Card>();
+        private List<Card> DealerHand = new List<Card>();
+        private int DealersHandTotal;
+        private int PlayersHandTotal;
+        private int DealerGameScore;
+        private int PlayerGameScore;
+
         TextView score;
         TextView handTotal;
         TextView convoText;
@@ -55,12 +65,124 @@ namespace BlackJack
 
         private void HitButton_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            PlayersHand.Add(Deck.RemoveTopCard());
+            //TODO implement a way to print card values
+            //PrintHand(PlayersHand);
+            PlayersHandTotal = UpdateScore(PlayersHand);
+            //TODO implement a way to update the players score in the score bar
+            //PrintPlayerScore();
         }
 
         private void StickButton_Click(object sender, EventArgs e)
         {
             throw new NotImplementedException();
+        }
+
+        private void NewGame()
+        {
+            Deck = new PlayingCardDeck();
+
+            Deck.Shuffle();
+
+            PlayersHandTotal = 0;
+            PlayersHand.Clear();
+
+            DealersHandTotal = 0;
+            DealerHand.Clear();
+
+            PlayersHand.Add(Deck.RemoveTopCard());
+            DealerHand.Add(Deck.RemoveTopCard());
+            PlayersHand.Add(Deck.RemoveTopCard());
+            DealerHand.Add(Deck.RemoveTopCard());
+
+            DealersHandTotal = UpdateScore(DealerHand);
+
+            //TODO implement a way to print card values
+            //PrintHand(PlayersHand);
+
+            PlayersHandTotal = UpdateScore(PlayersHand);
+
+            //TODO implement a way to update the players score in the score bar
+            //PrintPlayerScore();
+        }
+
+        private int UpdateScore(List<Card> hand)
+        {
+            int HandTotal = 0;
+            List<Card> aces = new List<Card>();
+
+            foreach (Card c in hand)
+            {
+                if (c.Value == 1)
+                {
+                    aces.Add(c);
+                }
+                else if (c.Value > 10)
+                {
+                    HandTotal += 10;
+                }
+                else
+                {
+                    HandTotal += c.Value;
+                }
+            }
+
+            return SetAceValue(aces, HandTotal);
+        }
+
+        private int SetAceValue(List<Card> aces, int total)
+        {
+            int HandTotal = total;
+
+            foreach (Card c in aces)
+            {
+                if (total + 11 > 21)
+                {
+                    HandTotal++;
+                }
+                else
+                {
+                    HandTotal += 11;
+                }
+            }
+
+            return HandTotal;
+        }
+
+        private void DealersTurn()
+        {
+            bool dealersTurn = true;
+
+            while (dealersTurn)
+            {
+                if (DealersHandTotal > PlayersHandTotal || DealersHandTotal == -1)
+                {
+                    dealersTurn = false;
+                }
+                else if (DealersHandTotal <= 16)
+                {
+                    DealerHand.Add(Deck.RemoveTopCard());
+                    DealersHandTotal = UpdateScore(DealerHand);
+                    CheckIfBust();
+                }
+                else
+                {
+                    dealersTurn = false;
+                }
+            }
+        }
+
+        private void CheckIfBust()
+        {
+            if (PlayersHandTotal > 21)
+            {
+                PlayersHandTotal = -1;
+            }
+
+            if (DealersHandTotal > 21)
+            {
+                DealersHandTotal = -1;
+            }
         }
     }
 }
