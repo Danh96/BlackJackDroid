6bf52d7d02ec04e036f518e33b763138228c64be
Daniel Hayes <daniel.hayes@aviva.com>
2018-04-25 11:28:50 +0100

    Implements splash screen

--- BlackJack/Resources/Resource.designer.cs
+++ BlackJack/Resources/Resource.designer.cs
@@ -80,6 +80,9 @@ namespace BlackJack
 			// aapt resource value: 0x7f02000b
 			public const int Spades = 2130837515;
 			
+			// aapt resource value: 0x7f02000c
+			public const int SplashScreen = 2130837516;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -93,92 +96,92 @@ namespace BlackJack
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060003
-			public const int BottomRightChar = 2131099651;
+			// aapt resource value: 0x7f070003
+			public const int BottomRightChar = 2131165187;
 			
-			// aapt resource value: 0x7f06001c
-			public const int ButtonGame = 2131099676;
+			// aapt resource value: 0x7f07001c
+			public const int ButtonGame = 2131165212;
 			
-			// aapt resource value: 0x7f06001b
-			public const int ButtonHit = 2131099675;
+			// aapt resource value: 0x7f07001b
+			public const int ButtonHit = 2131165211;
 			
-			// aapt resource value: 0x7f060006
-			public const int ButtonHome = 2131099654;
+			// aapt resource value: 0x7f070006
+			public const int ButtonHome = 2131165190;
 			
-			// aapt resource value: 0x7f06001a
-			public const int ButtonStick = 2131099674;
+			// aapt resource value: 0x7f07001a
+			public const int ButtonStick = 2131165210;
 			
-			// aapt resource value: 0x7f060011
-			public const int ConvoText = 2131099665;
+			// aapt resource value: 0x7f070011
+			public const int ConvoText = 2131165201;
 			
-			// aapt resource value: 0x7f060009
-			public const int DealerGameScore = 2131099657;
+			// aapt resource value: 0x7f070009
+			public const int DealerGameScore = 2131165193;
 			
-			// aapt resource value: 0x7f060010
-			public const int DealersFifthCard = 2131099664;
+			// aapt resource value: 0x7f070010
+			public const int DealersFifthCard = 2131165200;
 			
-			// aapt resource value: 0x7f06000c
-			public const int DealersFirstCard = 2131099660;
+			// aapt resource value: 0x7f07000c
+			public const int DealersFirstCard = 2131165196;
 			
-			// aapt resource value: 0x7f06000f
-			public const int DealersFourthCard = 2131099663;
+			// aapt resource value: 0x7f07000f
+			public const int DealersFourthCard = 2131165199;
 			
-			// aapt resource value: 0x7f06000b
-			public const int DealersHand = 2131099659;
+			// aapt resource value: 0x7f07000b
+			public const int DealersHand = 2131165195;
 			
-			// aapt resource value: 0x7f06000a
-			public const int DealersHandText = 2131099658;
+			// aapt resource value: 0x7f07000a
+			public const int DealersHandText = 2131165194;
 			
-			// aapt resource value: 0x7f06000d
-			public const int DealersSecondCard = 2131099661;
+			// aapt resource value: 0x7f07000d
+			public const int DealersSecondCard = 2131165197;
 			
-			// aapt resource value: 0x7f06000e
-			public const int DealersThirdCard = 2131099662;
+			// aapt resource value: 0x7f07000e
+			public const int DealersThirdCard = 2131165198;
 			
-			// aapt resource value: 0x7f060000
-			public const int LayoutCard = 2131099648;
+			// aapt resource value: 0x7f070000
+			public const int LayoutCard = 2131165184;
 			
-			// aapt resource value: 0x7f060005
-			public const int PlayAgain = 2131099653;
+			// aapt resource value: 0x7f070005
+			public const int PlayAgain = 2131165189;
 			
-			// aapt resource value: 0x7f060008
-			public const int PlayerGameScore = 2131099656;
+			// aapt resource value: 0x7f070008
+			public const int PlayerGameScore = 2131165192;
 			
-			// aapt resource value: 0x7f060018
-			public const int PlayersFifthCard = 2131099672;
+			// aapt resource value: 0x7f070018
+			public const int PlayersFifthCard = 2131165208;
 			
-			// aapt resource value: 0x7f060014
-			public const int PlayersFirstCard = 2131099668;
+			// aapt resource value: 0x7f070014
+			public const int PlayersFirstCard = 2131165204;
 			
-			// aapt resource value: 0x7f060017
-			public const int PlayersFourthCard = 2131099671;
+			// aapt resource value: 0x7f070017
+			public const int PlayersFourthCard = 2131165207;
 			
-			// aapt resource value: 0x7f060013
-			public const int PlayersHand = 2131099667;
+			// aapt resource value: 0x7f070013
+			public const int PlayersHand = 2131165203;
 			
-			// aapt resource value: 0x7f060012
-			public const int PlayersHandText = 2131099666;
+			// aapt resource value: 0x7f070012
+			public const int PlayersHandText = 2131165202;
 			
-			// aapt resource value: 0x7f060015
-			public const int PlayersSecondCard = 2131099669;
+			// aapt resource value: 0x7f070015
+			public const int PlayersSecondCard = 2131165205;
 			
-			// aapt resource value: 0x7f060016
-			public const int PlayersThirdCard = 2131099670;
+			// aapt resource value: 0x7f070016
+			public const int PlayersThirdCard = 2131165206;
 			
-			// aapt resource value: 0x7f060007
-			public const int ScoreBar = 2131099655;
+			// aapt resource value: 0x7f070007
+			public const int ScoreBar = 2131165191;
 			
-			// aapt resource value: 0x7f060002
-			public const int SuitImage = 2131099650;
+			// aapt resource value: 0x7f070002
+			public const int SuitImage = 2131165186;
 			
-			// aapt resource value: 0x7f060001
-			public const int TopLeftChar = 2131099649;
+			// aapt resource value: 0x7f070001
+			public const int TopLeftChar = 2131165185;
 			
-			// aapt resource value: 0x7f060004
-			public const int WinnerText = 2131099652;
+			// aapt resource value: 0x7f070004
+			public const int WinnerText = 2131165188;
 			
-			// aapt resource value: 0x7f060019
-			public const int bottom_panel = 2131099673;
+			// aapt resource value: 0x7f070019
+			public const int bottom_panel = 2131165209;
 			
 			static Id()
 			{
@@ -243,6 +246,28 @@ namespace BlackJack
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060001
+			public const int MyTheme = 2131099649;
+			
+			// aapt resource value: 0x7f060000
+			public const int MyTheme_Base = 2131099648;
+			
+			// aapt resource value: 0x7f060002
+			public const int MyTheme_Splash = 2131099650;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
---
+++ BlackJack/SplashScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+
+namespace BlackJack
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true, Icon = "@drawable/AppLogo", Label = "Black Jack", ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashActivity : Activity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { AppStartup(); });
+            startupWork.Start();
+        }
+
+        async Task AppStartup()
+        {
+            await Task.Delay(3000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
